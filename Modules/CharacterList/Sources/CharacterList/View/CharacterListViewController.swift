4d88afd0f765c72b1a641e388e937fbd85b14133
shimaacc <shimaa.elcc@gmail.com>
2024-10-25 02:32:23 +0300

    adjust character list view

--- Modules/CharacterList/Sources/CharacterList/View/CharacterListViewController.swift
+++ Modules/CharacterList/Sources/CharacterList/View/CharacterListViewController.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+import Combine
 
+@available(iOS 16.0, *)
 class CharacterListViewController: UIViewController {
-    @IBOutlet private var tableView: UITableView!
     private var viewModel: CharacterListViewModelProtocol
-    
+    public var cancellables: Set<AnyCancellable> = .init()
+
+    let tableView = UITableView()
+
+    //TODO: add this in builder
     init(viewModel: CharacterListViewModelProtocol) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -22,22 +27,48 @@ class CharacterListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        setupTableView()
+        bind()
     }
+    
+    func bind() {
+        //TODO: replace sink, dispatch with uisink
+        viewModel.uiModel.$characterList.sink { [weak self] list in
+            guard let self else { return }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }.store(in: &cancellables)
 
-
+    }
 }
 
-private extension CharacterListViewController {
-//    func setupTableView() {
-//        tableView.register(
-//            UINib(nibName: CharacterTableViewCell.reuseIdentifier, bundle: nil),
-//            forCellReuseIdentifier: CharacterTableViewCell.reuseIdentifier
-//        )
-//        tableView.separatorStyle = .none
-//    }
-    
-    private func bind() {
-    }
+@available(iOS 16.0, *)
 
+extension CharacterListViewController: UITableViewDataSource, UITableViewDelegate {
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.register(CharacterTableViewCell.self, forCellReuseIdentifier: CharacterTableViewCell.reuseIdentifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.uiModel.characterList.count
+        }
+        
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CharacterTableViewCell", for: indexPath) as! CharacterTableViewCell
+            let character = viewModel.uiModel.characterList[indexPath.row]
+            cell.characterImage = "star.fill"
+            cell.characterName = character.title
+            cell.characterStatus = character.species
+            return cell
+        }
 }
--- Modules/CharacterList/Sources/CharacterList/View/CharacterTableViewCell.swift
+++ Modules/CharacterList/Sources/CharacterList/View/CharacterTableViewCell.swift
@@ -8,14 +8,62 @@
 import UIKit
 import SwiftUI
 
+@available(iOS 16.0, *)
 class CharacterTableViewCell: UITableViewCell {
-//    init() {
-//        self.contentConfiguration = UIHostingConfiguration(content: {
-//            if #available(iOS 13.0, *) {
-//                Text("shimaaaaaa")
-//            } else {
-//                // Fallback on earlier versions
-//            }
-//        })
-//    }
+    static let reuseIdentifier = "CharacterTableViewCell"
+    
+    var characterImage: String? {
+         didSet {
+             updateUI()
+         }
+     }
+     
+     var characterName: String? {
+         didSet {
+             updateUI()
+         }
+     }
+     
+     var characterStatus: String? {
+         didSet {
+             updateUI()
+         }
+     }
+     
+     private func updateUI() {
+         guard let image = characterImage, let name = characterName, let status = characterStatus else { return }
+         
+         self.contentConfiguration = UIHostingConfiguration {
+             CharacterRowView(image: image, name: name, status: status)
+         }
+     }
+ }
+
+
+struct CharacterRowView: View {
+    let image: String
+    let name: String
+    let status: String
+
+    var body: some View {
+        HStack {
+            Image(systemName: image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50, height: 50)
+                .clipShape(Circle())
+                .padding(.trailing, 10)
+
+            VStack(alignment: .leading) {
+                Text(name)
+                    .font(.headline)
+                Text(status)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+
+            Spacer()
+        }
+        .padding()
+    }
 }
